be310e9679abf92648f56d474bb7cfec5ad6dc81
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-04 08:57:32 +0000

    pearson-pdf: split code into functions

--- pearson-pdf/__main__.py
+++ pearson-pdf/__main__.py
@@ -5,7 +5,6 @@ from PIL import Image
 
 
 textbook_id = ""
-output_path = "output.pdf"
 
 
 def textbook_url(id: str):
@@ -16,24 +15,32 @@ def textbook_url(id: str):
     )
 
 
-with TemporaryDirectory() as tmpdir:
-    page = 0
-    pages = []
-    while True:
-        print("downloading page " + str(page))
-        response = requests.get(textbook_url(textbook_id) + str(page))
-        if response.status_code == 200:
-            pages.append(Image.open(BytesIO(response.content)))
-            page += 1
-        elif response.status_code == 403:
-            print("got 403 for page " + str(page) + ", not downloading anymore pages")
-            break
-        else:
-            raise Exception('unexpected status code "' + response.status_code + '"')
+def download_pages():
+    with TemporaryDirectory() as tmpdir:
+        page = 0
+        pages = []
+        while True:
+            print("downloading page " + str(page))
+            response = requests.get(textbook_url(textbook_id) + str(page))
+            if response.status_code == 200:
+                pages.append(Image.open(BytesIO(response.content)))
+                page += 1
+            elif response.status_code == 403:
+                print(
+                    "got 403 for page " + str(page) + ", not downloading anymore pages"
+                )
+                break
+            else:
+                raise Exception('unexpected status code "' + response.status_code + '"')
+    return pages
 
+
+def combine_pages(pages: list, path: str):
     page_0 = pages[0]
     pages.pop(0)
     print('saving pdf to "' + output_path + '"')
-    page_0.save(
-        output_path, "PDF", resolution=100.0, save_all=True, append_images=pages
-    )
+    page_0.save(path, "PDF", resolution=100.0, save_all=True, append_images=pages)
+
+
+if __name__ == "__main__":
+    combine_pages(download_pages(), "output.pdf")
