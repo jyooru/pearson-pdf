31a9b18e41089b33e3f7a63894a8ea52c130256c
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-04 13:03:58 +0000

    tests: create test for pearson_pdf.download_pages

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,14 @@
-from pearson_pdf import combine_pages
+from pearson_pdf import combine_pages, download_pages
 import requests
 from io import BytesIO
 from PIL import Image
 from tempfile import TemporaryDirectory
 import os
+from dotenv import load_dotenv
+import pytest
+
+
+load_dotenv()
 
 
 def test_combine_pages():
@@ -17,3 +22,12 @@ def test_combine_pages():
         path = tmpdir + "/output.pdf"
         combine_pages(images, path)
         assert os.path.exists(path)
+
+
+@pytest.mark.skipif(os.getenv("book_id") is None, reason="book_id is not set")
+def test_download_pages():
+    pages = download_pages(os.getenv("book_id"))
+    assert len(pages) != 0
+    for page in pages:
+        assert page.height != 0
+        assert page.width != 0
