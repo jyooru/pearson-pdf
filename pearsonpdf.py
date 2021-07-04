a47ddc11952de02dfe0eb3a2beb27a10e867ff11
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-04 18:39:12 +1000

    add old code

---
+++ pearsonpdf.py
@@ -0,0 +1,41 @@
+import requests
+import tempfile
+import io
+from PIL import Image
+
+
+textbook_id = ""
+output_path = "output.pdf"
+
+
+def textbook_url(id: str, page: str):
+    return (
+        "https://d2f01w1orx96i0.cloudfront.net/resources/products/epubs/generated/"
+        + textbook_id
+        + "/foxit-assets/pages/page"
+        + page
+        + "?password=&accessToken=null&formMode=true"
+    )
+
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    page = 0
+    pages = []
+    while True:
+        print("downloading page " + str(page))
+        response = requests.get(textbook_url(textbook_id, str(page)))
+        if response.status_code == 200:
+            pages.append(Image.open(io.BytesIO(response.content)))
+            page += 1
+        elif response.status_code == 403:
+            print("got 403 for page " + str(page) + ", not downloading anymore pages")
+            break
+        else:
+            raise Exception('unexpected status code "' + response.status_code + '"')
+
+    page_0 = pages[0]
+    pages.pop(0)
+    print('saving pdf to "' + output_path + '"')
+    page_0.save(
+        output_path, "PDF", resolution=100.0, save_all=True, append_images=pages
+    )
