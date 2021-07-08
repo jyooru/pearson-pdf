969613e5defdd279851da9b20db49349958344dc
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-08 13:20:18 +1000

    pearson_pdf: main: implement only printing book id or url

--- pearson_pdf/__main__.py
+++ pearson_pdf/__main__.py
@@ -1,4 +1,4 @@
-from . import get_book_id, combine_pages, download_pages
+from . import get_book_id, get_book_url, combine_pages, download_pages
 import argparse
 
 
@@ -8,8 +8,17 @@ def main():
     )
     parser.add_argument("book", type=str, help="Book ID or URL")
     parser.add_argument("save_path", type=str, help="Path to save PDF")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-u", "--url", action="store_true", help="Print Book URL")
+    group.add_argument("-i", "--id", action="store_true", help="Print Book ID")
     args = parser.parse_args()
 
+    if args.url:
+        print(get_book_url(get_book_id(args.book)))
+        exit()
+    if args.id:
+        print(get_book_id(get_book_url(args.book)))
+        exit()
     args.book = get_book_id(args.book)
     combine_pages(download_pages(args.book), args.save_path)
 
