dfbbd620c92e1eb26fb0541df411292fdb91d913
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-08 13:34:02 +1000

    pearson_pdf: cli: make save_path optional

--- pearson_pdf/__main__.py
+++ pearson_pdf/__main__.py
@@ -7,20 +7,22 @@ def main():
         prog="pearson_pdf", description="Download Pearson books as PDFs."
     )
     parser.add_argument("book", type=str, help="Book ID or URL")
-    parser.add_argument("save_path", type=str, help="Path to save PDF")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-u", "--url", action="store_true", help="Print Book URL")
     group.add_argument("-i", "--id", action="store_true", help="Print Book ID")
+    group.add_argument("save_path", type=str, help="Path to save PDF", nargs="?")
     args = parser.parse_args()
-
     if args.url:
         print(get_book_url(get_book_id(args.book)))
         exit()
-    if args.id:
+    elif args.id:
         print(get_book_id(get_book_url(args.book)))
         exit()
-    args.book = get_book_id(args.book)
-    combine_pages(download_pages(args.book), args.save_path)
+    else:
+        if args.save_path is None:
+            args.save_path = get_book_id(get_book_url(args.book)) + ".pdf"
+        args.book = get_book_id(args.book)
+        combine_pages(download_pages(args.book), args.save_path)
 
 
 if __name__ == "__main__":
