2e0e62c9e57f403048795ee48b643636c3ceb863
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-08 14:39:38 +1000

    setup: read file instead of executing it

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup, find_packages
-from pearson_pdf import __version__
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 
 with open("README.md", "r", encoding="utf-8") as readme:
@@ -8,7 +24,7 @@ with open("README.md", "r", encoding="utf-8") as readme:
 
 setup(
     name="pearson-pdf",
-    version=__version__,
+    version=get_version("pearson_pdf/__init__.py"),
     author="jyooru",
     license="MIT",
     description="Tool to download Pearson books as PDFs.",
