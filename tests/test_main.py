1092ab84176ba522f29b3a3756b6ce243e3f134a
Joel <joel@joel.tokyo>
2021-09-19 12:18:00 +1000

    style: run isort

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,10 @@
 from itertools import combinations
-from pearson_pdf import PageDownloadError
 
 import pytest
 from dotenv import load_dotenv
 from faker import Faker
 
+from pearson_pdf import PageDownloadError
 from pearson_pdf.__main__ import parse_args, run
 
 
