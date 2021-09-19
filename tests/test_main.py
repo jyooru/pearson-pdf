df4baf2e50a904a97cf1fbff316ee11be8d477a5
Joel <joel@joel.tokyo>
2021-09-19 11:46:37 +1000

    test: implement tests for parse_args

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
+import itertools
 import pytest
 from dotenv import load_dotenv
 from faker import Faker
-
+from itertools import combinations
 from pearson_pdf.__main__ import parse_args, run
 
 
@@ -14,3 +15,18 @@ def faker():
 
 
 def test_parse_args():
+    with pytest.raises(SystemExit):
+        parse_args()
+
+    parse_args(["qwerty"])
+    parse_args(["qwerty", "-i"])
+    parse_args(["qwerty", "-u"])
+
+    mutually_exclusive_args = ["-u", "-i", "book.pdf"]
+    mutually_exclusive_combinations = combinations(mutually_exclusive_args, 2)
+    for combination in mutually_exclusive_combinations:
+        with pytest.raises(SystemExit):
+            parse_args(list(combination).insert(0, "qwerty"))
+
+    with pytest.raises(SystemExit):
+        parse_args(["--version"])
