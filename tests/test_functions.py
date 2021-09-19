c89a42fc6bf60d37fe66ffc114e3142352270214
Joel <joel@joel.tokyo>
2021-09-19 01:01:13 +0000

    fix(typing): missing type annotations

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -21,17 +21,17 @@ load_dotenv()
 
 
 @pytest.fixture
-def faker():
+def faker() -> Faker:
     return Faker()
 
 
-def test_get_book(faker):
+def test_get_book(faker: Faker) -> None:
     book_id = faker.uuid4()
     assert get_book_id(get_book_url(book_id)) == book_id
 
 
-def test_combine_pages():
-    def random_images():
+def test_combine_pages() -> None:
+    def random_images() -> Image:
         return Image.open(
             BytesIO(requests.get("https://source.unsplash.com/random").content)
         )
@@ -44,7 +44,7 @@ def test_combine_pages():
 
 
 @pytest.mark.skipif(os.getenv("book_id") is None, reason="book_id is not set")
-def test_download_pages():
+def test_download_pages() -> None:
     max_pages = 10
     pages = download_pages(os.getenv("book_id"), max_pages)
     assert len(pages) == max_pages
